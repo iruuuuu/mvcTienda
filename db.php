1b2fb096b68415b1bc8ff93c75634a42af203d65
Samuel-lar-enr <samuellaraenr@gmail.com>
2025-10-20 12:44:15 +0200

    Done pedido repository

--- db.php
+++ db.php
@@ -3,7 +3,7 @@ class Connection
 {
     public static function connect()
     {
-        $connect = new mysqli("localhost", "root", "", "foro");
+        $connect = new mysqli("localhost", "root", "", "tienda");
         $connect->query("SET NAMES 'utf8'");
         $connect->autocommit(true);
         return $connect;
--- models/Product.php
+++ models/Product.php
@@ -1,6 +1,6 @@
 <?php
 
-class productos {
+class product {
     private $id;
     private $name;
     private $description;
