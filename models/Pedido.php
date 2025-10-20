36a02511dd79da8a6d2e63690b39bc4bdf3a745f
Samuel-lar-enr <samuellaraenr@gmail.com>
2025-10-20 12:15:42 +0200

    Done pedido class

---
+++ models/Pedido.php
@@ -0,0 +1,28 @@
+<?php
+
+class pedido{
+    private $id;
+    private $productId
+    private $userId
+
+    public function __construct($id, $productId, $userId){
+        $this->id = $id;
+        $this->productId = $productId;
+        $this->userId = $userId;
+    }
+
+    public function getId(){
+        return $this->id;
+    }
+
+    public function getProductId(){
+        return $this->productId;
+    }
+
+    public function getUserId(){
+        return $this->userId;
+    }
+
+}
+
+
